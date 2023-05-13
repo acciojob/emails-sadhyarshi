4c6b354d8ec5ecba1ea2f7d192ea5b54fa3fd5f9
sadhyarshi <sadhyarshii@gmail.com>
2023-05-13 15:09:27 +0530

    done

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -11,4 +11,14 @@ public class Meeting {
         this.endTime = endTime;
     }
 
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+
 }
